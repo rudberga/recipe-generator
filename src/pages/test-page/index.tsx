90cdc054231d71bd3c660c97974a43378f9d0f22
Alex <56194510+rudberga@users.noreply.github.com>
2024-02-25 22:01:30 +0900

    DELETE - test page

--- src/pages/test-page/index.tsx
+++
@@ -1,9 +0,0 @@
-// import Head from 'next/head'
-
-import TestComponent from "@/components/test-component/test-component";
-
-export default function Test() {
-  return (
-    <TestComponent />
-  )
-}
