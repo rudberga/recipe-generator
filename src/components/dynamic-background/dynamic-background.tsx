5ebbd58e0a402f51a031110ff05fe1061b6d00aa
Alex <56194510+rudberga@users.noreply.github.com>
2024-02-25 15:22:23 +0900

    ADD - dynamic background blob start

---
+++ src/components/dynamic-background/dynamic-background.tsx
@@ -0,0 +1,22 @@
+// DynamicBackground.tsx
+import React, { FC } from 'react'
+import classes from './dynamic-background.module.scss'
+
+// interface Props {
+// 	isActive: boolean
+// }
+
+const DynamicBackground: FC = () => {
+	// const backgroundImage = isActive
+	// 	? '/images/activeBackground.svg'
+	// 	: '/images/defaultBackground.svg'
+
+	return (
+		<div
+			className={classes.BackgroundStyle}
+			// style={{ backgroundImage: `url(${backgroundImage})` }}
+		></div>
+	)
+}
+
+export default DynamicBackground
