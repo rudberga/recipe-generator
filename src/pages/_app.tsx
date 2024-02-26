53c06ddaff32b027b1d3fb5d7e46bf2d55e56847
rudberga <rudberg@pm.me>
2024-02-26 05:58:53 +0000

    ADD - mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,19 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { queryClient } from '../lib/reactQuery';
 import { QueryClientProvider } from "@tanstack/react-query";
+import '@mantine/core/styles.css';
+import { createTheme, MantineProvider } from '@mantine/core';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <MantineProvider theme={theme}>
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
     </QueryClientProvider>
+    </MantineProvider>
   );
 }
