9dcdbbde6d1e16bb220aa4da4646259247f68d38
rudberga <rudberg@pm.me>
2024-02-26 05:05:16 +0000

    UPDATE - font import location

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,10 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { queryClient } from '../lib/reactQuery';
 import { QueryClientProvider } from "@tanstack/react-query";
-import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
-      </Head>
       <Component {...pageProps} />
     </QueryClientProvider>
   );
