df46c320c55339f6a7c318552a5d5ab0ede30afa
rudberga <rudberg@pm.me>
2024-02-28 06:24:50 +0000

    ADD - stepper

--- src/components/questions-card/questions-card.tsx
+++ src/components/questions-card/questions-card.tsx
@@ -1,11 +1,34 @@
-import { FC } from "react"
+import { FC, useState } from "react"
 import classes from './questions-card.module.scss'
 import IngredientsStep from "./ingredients-step/ingredients-step"
+import { Button, Group, Stepper } from "@mantine/core";
 
 const QuestionsCard: FC = () => {
+    const [active, setActive] = useState(0);
+    const nextStep = () => setActive((current) => (current < 3 ? current + 1 : current));
+    const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current));
+
     return (
         <div className={classes.QuestionsCardContainer}>
-            <IngredientsStep />
+            <Stepper active={active} onStepClick={setActive}>
+                <Stepper.Step label="First step" description="Dietary">
+                    Step 1 content: Dietary
+                </Stepper.Step>
+                <Stepper.Step label="Second step" description="Preferences">
+                    Step 2 content: Preferences
+                </Stepper.Step>
+                <Stepper.Step label="Final step" description="Ingredients">
+                    Step 3 content: <IngredientsStep />
+                </Stepper.Step>
+                <Stepper.Completed>
+                    Completed, click back button to get to previous step
+                </Stepper.Completed>
+            </Stepper>
+
+            <Group justify="center" mt="xl">
+                <Button variant="default" onClick={prevStep}>Back</Button>
+                <Button onClick={nextStep}>Next step</Button>
+            </Group>
         </div>
     )
 }
