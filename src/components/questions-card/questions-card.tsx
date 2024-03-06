ebcf749f742a03fb10ef4100f2d5c2a4f70e91c7
Alex <56194510+rudberga@users.noreply.github.com>
2024-03-06 22:52:50 +0900

    ADD - basic styling questions card

--- src/components/questions-card/questions-card.tsx
+++ src/components/questions-card/questions-card.tsx
@@ -1,38 +1,46 @@
-import { FC, useState } from "react"
-import { Button, Group, Stepper } from "@mantine/core";
+import { FC, useState } from 'react'
+import { Button, Group, Stepper } from '@mantine/core'
 import classes from './questions-card.module.scss'
-import IngredientsStep from "./ingredients-step/ingredients-step"
-import DietaryStep from "./dietary-step/dietary-step";
-import PreferencesStep from "./preferences-step/preferences-step";
+import IngredientsStep from './ingredients-step/ingredients-step'
+import DietaryStep from './dietary-step/dietary-step'
+import PreferencesStep from './preferences-step/preferences-step'
 
 const QuestionsCard: FC = () => {
-    const [active, setActive] = useState(0);
-    const nextStep = () => setActive((current) => (current < 3 ? current + 1 : current));
-    const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current));
+	const [active, setActive] = useState(0)
+	const nextStep = () =>
+		setActive((current) => (current < 3 ? current + 1 : current))
+	const prevStep = () =>
+		setActive((current) => (current > 0 ? current - 1 : current))
 
-    return (
-        <div className={classes.QuestionsCardContainer}>
-            <Stepper active={active} onStepClick={setActive} allowNextStepsSelect={false}>
-                <Stepper.Step label="First step" description="Dietary">
-                    <DietaryStep />
-                </Stepper.Step>
-                <Stepper.Step label="Second step" description="Preferences">
-                    <PreferencesStep />
-                </Stepper.Step>
-                <Stepper.Step label="Final step" description="Ingredients">
-                    <IngredientsStep />
-                </Stepper.Step>
-                <Stepper.Completed>
-                    LOADER HERE THEN SWITCH TO RESULT PAGE COMPONENT
-                </Stepper.Completed>
-            </Stepper>
+	return (
+		<div className={classes.QuestionsCardContainer}>
+			<Stepper
+				active={active}
+				onStepClick={setActive}
+				allowNextStepsSelect={false}
+			>
+				<Stepper.Step label='First step' description='Dietary'>
+					<DietaryStep />
+				</Stepper.Step>
+				<Stepper.Step label='Second step' description='Preferences'>
+					<PreferencesStep />
+				</Stepper.Step>
+				<Stepper.Step label='Final step' description='Ingredients'>
+					<IngredientsStep />
+				</Stepper.Step>
+				<Stepper.Completed>
+					LOADER HERE THEN SWITCH TO RESULT PAGE COMPONENT
+				</Stepper.Completed>
+			</Stepper>
 
-            <Group justify="center" mt="xl">
-                <Button variant="default" onClick={prevStep}>Back</Button>
-                <Button onClick={nextStep}>Next step</Button>
-            </Group>
-        </div>
-    )
+			<Group justify='center' mt='xl'>
+				<Button variant='default' onClick={prevStep}>
+					Back
+				</Button>
+				<Button onClick={nextStep}>Next step</Button>
+			</Group>
+		</div>
+	)
 }
 
 export default QuestionsCard
