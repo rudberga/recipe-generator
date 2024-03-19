391cbc5b1216ff48c5692d58a1d8a6017c912b63
rudberga <rudberg@pm.me>
2024-03-19 04:50:56 +0000

    UPDATE - refactor multiselect to a component

---
+++ src/components/questions-card/form-multiselect/form-multiselect.tsx
@@ -0,0 +1,71 @@
+import { FC } from "react";
+import { MultiSelect, Pill, PillGroup } from "@mantine/core";
+import { UseFormReturnType } from "@mantine/form";
+import { FormValues } from "../questions-card";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import classes from './form-multiselect.module.scss';
+
+interface FormMultiselectProps {
+    valuesInputForm: UseFormReturnType<FormValues>;
+    data: string[];
+    label: string;
+    placeholder: string;
+    nothingFoundMessage: string;
+    field: keyof FormValues;
+}
+
+const FormMultiselect: FC<FormMultiselectProps> = ({
+    valuesInputForm,
+    data,
+    label,
+    placeholder,
+    nothingFoundMessage,
+    field,
+}) => {
+    const handleRemoveClick = (valueToRemove: string) => {
+        const newValues = valuesInputForm.values[field].filter(value => value !== valueToRemove);
+        valuesInputForm.setFieldValue(field, newValues);
+    };
+
+    return (
+        <div className={classes.StepContainer}>
+            <div className={classes.TopSection}>
+                <p>{label}</p>
+                <p>{placeholder}</p>
+            </div>
+            <div className={classes.SelectPillsContainer}>
+                <div>
+                    <MultiSelect
+                        placeholder={placeholder}
+                        data={data}
+                        nothingFoundMessage={nothingFoundMessage}
+                        rightSection={<FaMagnifyingGlass />}
+                        {...valuesInputForm.getInputProps(field)}
+                        classNames={{
+                            input: classes.SearchInput,
+                            pill: classes.SearchPill,
+                        }}
+                    />
+                </div>
+                <div>
+                    <PillGroup>
+                        {valuesInputForm.values[field].map((value) => (
+                            <Pill
+                                key={value}
+                                size="md"
+                                withRemoveButton
+                                onRemove={() => handleRemoveClick(value)}
+                                classNames={{ root: classes.PillRoot }}
+                            >
+                                {value}
+                            </Pill>
+                        ))}
+                    </PillGroup>
+                </div>
+            </div>
+            <div>SUGGESTIONS DOWN HERE</div>
+        </div>
+    );
+};
+
+export default FormMultiselect;
--- src/components/questions-card/ingredients-step/ingredients-step.tsx
+++ src/components/questions-card/ingredients-step/ingredients-step.tsx
@@ -1,67 +1,28 @@
-import { FC, SetStateAction, useState } from "react"
-import classes from './ingredients-step.module.scss'
-import { Badge, Group, MultiSelect, Pill, PillGroup } from "@mantine/core"
-import { FaMagnifyingGlass } from "react-icons/fa6";
+import { FC } from "react"
+// import classes from '../form-multiselect/form-multiselect.module.scss'
 import { UseFormReturnType } from "@mantine/form";
 import { FormValues } from "../questions-card";
+import FormMultiselect from "../form-multiselect/form-multiselect";
 
 interface FormInputProps {
-    valuesInputForm: UseFormReturnType<FormValues>
+    valuesInputForm: UseFormReturnType<FormValues>;
     errors: any
 }
 
-const IngredientsStep: FC<FormInputProps> = ({
+const IngredientsStep: FC<FormInputProps> = ({ 
     valuesInputForm,
-    errors,
+    errors, 
 }) => {
-    // Handler for removing a specific pill
-    const handleRemoveClick = (valueToRemove: string) => {
-        const newValues = valuesInputForm.values.ingredients.filter(value => value !== valueToRemove);
-        valuesInputForm.setFieldValue('ingredients', newValues);
-    };
-
-    console.log("VALUES:", valuesInputForm.getInputProps('ingredients'))
-
     return (
-        <div className={classes.IngredientsStepContainer}>
-            <div className={classes.TopSection}>
-                <p>Ingredienser</p>
-                <p>Välj dina ingredienser nedanför så sköter vi resten! :)</p>
-            </div>
-            <div className={classes.SelectPillsContainer}>
-                <div>
-                    <MultiSelect
-                        placeholder="Pick value"
-                        data={['Mjölk', 'Ägg', 'Smör', 'Socker']}
-                        nothingFoundMessage="Oops, vi hittade inget..."
-                        searchable
-                        rightSection={<FaMagnifyingGlass />}
-                        classNames={{
-                            input: classes.SearchIngredientsInput,
-                            pill: classes.SearchIngredientsPill,
-                        }}
-                        {...valuesInputForm.getInputProps('ingredients')}
-                    />
-                </div>
-                <div>
-                    <PillGroup>
-                        {valuesInputForm.values.ingredients.map((value) => (
-                            <Pill
-                                key={value}
-                                size="md"
-                                withRemoveButton
-                                classNames={{ root: classes.PillRoot }}
-                                onRemove={() => handleRemoveClick(value)}
-                            >
-                                {value}
-                            </Pill>
-                        ))}
-                    </PillGroup>
-                </div>
-            </div>
-            <div>SUGGESTIONS DOWN HERE</div>
-        </div>
-    )
-}
+        <FormMultiselect
+            valuesInputForm={valuesInputForm}
+            data={['Mjölk', 'Ägg', 'Smör', 'Socker']}
+            label="Ingredienser"
+            placeholder="Välj dina ingredienser"
+            nothingFoundMessage="Oops, vi hittade inget..."
+            field="ingredients"
+        />
+    );
+};
 
-export default IngredientsStep
+export default IngredientsStep;
