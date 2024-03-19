00fbb5a7dc537f5c77771ebb7c8262ffd2c42a18
rudberga <rudberg@pm.me>
2024-03-19 09:17:05 +0000

    ADD - fetch dietary options

--- src/components/questions-card/dietary-step/dietary-step.tsx
+++ src/components/questions-card/dietary-step/dietary-step.tsx
@@ -1,21 +1,51 @@
 import { UseFormReturnType } from "@mantine/form";
-import { FC } from "react"
+import { FC, useEffect, useState } from "react";
 import { FormValues } from "../questions-card";
 import FormMultiselect from "../form-multiselect/form-multiselect";
 
-interface FormInputProps {
-    valuesInputForm: UseFormReturnType<FormValues>;
-    errors: any
+interface DietaryOptions {
+    id: number;
+    diet_option: string;
 }
 
-const DietaryStep: FC<FormInputProps> = ({ 
-    valuesInputForm,
-    errors, 
-}) => {
+interface FormInputProps {
+    valuesInputForm: UseFormReturnType<FormValues>;
+    errors: any;
+}
+
+const DietaryStep: FC<FormInputProps> = ({ valuesInputForm, errors }) => {
+    const [dietaryOptions, setDietaryOptions] = useState<string[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            try {
+                const response = await fetch('/api/fetchSupabaseDietaryOptions');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const options: DietaryOptions[] = await response.json();
+                setDietaryOptions(options.map(option => option.diet_option));
+            } catch (err) {
+                setError('Failed to fetch dietary options');
+                console.error(err);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    // if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error}</div>;
+
     return (
         <FormMultiselect
             valuesInputForm={valuesInputForm}
-            data={['Vegan', 'Vegetarian', 'Halal', 'Glutenintolerant']}
+            data={dietaryOptions}
             label="Diet"
             placeholder="Välj din diet"
             nothingFoundMessage="Oops, vi hittade inget..."
@@ -24,4 +54,4 @@ const DietaryStep: FC<FormInputProps> = ({
     );
 };
 
-export default DietaryStep
+export default DietaryStep;
