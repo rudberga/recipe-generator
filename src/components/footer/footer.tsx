94540957a1b74059878be59094ac783f0fd55fe6
rudberga <rudberg@pm.me>
2024-03-26 08:37:15 +0000

    ADD - footer start

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react"
+import classes from './footer.module.scss'
+
+const Footer: FC = () => {
+    return (
+        <footer className={classes.FooterContainer}>FOOTER WILL BE HERE</footer>
+    )
+}
+
+export default Footer
